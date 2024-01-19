9dd1e25250ddceadc7deb29d7c4a1053be85e245
Alexandra <kobatoha@gmail.com>
2024-01-19 23:58:11 +0300

    add config with connect to db information

---
+++ config.py
@@ -0,0 +1,8 @@
+# PostgreSQL db information
+host = 'localhost'
+user = 'postgres'
+password = 'postgres'
+db_name = 'fastapi_menu'
+port = 5433
+
+DB_URL = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
