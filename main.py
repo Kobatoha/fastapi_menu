59152812b2e92008172ada978730388fe908aab3
Alexandra <kobatoha@gmail.com>
2024-01-21 23:57:18 +0300

    add get_menus and uuid

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import DB_URL
 from pydantic import BaseModel
+import uuid
 
 app = FastAPI(
     title='Menu App'
@@ -20,12 +21,20 @@ class MenuCreate(BaseModel):
     description: str
 
 
-@app.post('/api/v1/menus')
+@app.post('/api/v1/menus', status_code=201)
 async def create_menu(menu: MenuCreate):
+    with Session() as session:
+        menu_id = str(uuid.uuid4())
+        db_menu = Menu(id=menu_id, title=menu.title, description=menu.description)
+        session.add(db_menu)
+        session.commit()
+        session.refresh(db_menu)
+        return db_menu.__dict__
+
+
+@app.get('/api/v1/menus')
+async def get_menus():
     session = Session()
-    db_menu = Menu(title=menu.title, description=menu.description)
-    session.add(db_menu)
-    session.commit()
-    session.refresh(db_menu)
+    menus = session.query(Menu).all()
     session.close()
-    return db_menu
+    return menus
