92eb00c7d708101d9ed3018eb7c6f9cf28ae3d5a
Alexandra <kobatoha@gmail.com>
2024-01-21 18:18:37 +0300

    add post create_menu

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from DataBase.Base import Base
+from DataBase.Menu import Menu, Submenu, Dish
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import DB_URL
+from pydantic import BaseModel
 
 app = FastAPI(
     title='Menu App'
 )
 
+engine = create_engine(DB_URL)
 
-@app.get('/users/{user_id}')
-def get_user(user_id):
-    return user_id
+Session = sessionmaker(bind=engine)
+
+
+class MenuCreate(BaseModel):
+    title: str
+    description: str
+
+
+@app.post('/api/v1/menus')
+async def create_menu(menu: MenuCreate):
+    session = Session()
+    db_menu = Menu(title=menu.title, description=menu.description)
+    session.add(db_menu)
+    session.commit()
+    session.refresh(db_menu)
+    session.close()
+    return db_menu
