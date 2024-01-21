3eb52142e9223802dc56d6337f2183e883d0ff69
Alexandra <kobatoha@gmail.com>
2024-01-21 23:56:37 +0300

    fix id Integer -> String (uuid)

--- DataBase/Menu.py
+++ DataBase/Menu.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Numeric
 from sqlalchemy.orm import relationship
 from DataBase.Base import Base
+import uuid
 from config import DB_URL
 
 engine = create_engine(DB_URL)
@@ -9,7 +10,7 @@ engine = create_engine(DB_URL)
 class Menu(Base):
     __tablename__ = 'menu'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String, default=str(uuid.uuid4()), primary_key=True)
     title = Column(String, nullable=False)
     description = Column(String)
     submenu = relationship('Submenu', back_populates='menu', cascade='all, delete-orphan')
@@ -18,9 +19,9 @@ class Menu(Base):
 class Submenu(Base):
     __tablename__ = 'submenu'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String, default=str(uuid.uuid4()), primary_key=True)
     name = Column(String, nullable=False)
-    menu_id = Column(Integer, ForeignKey('menu.id'), nullable=False)
+    menu_id = Column(String, ForeignKey('menu.id'), nullable=False)
     menu = relationship('Menu', back_populates='submenu')
     dishes = relationship('Dish', back_populates='submenu', cascade='all, delete-orphan')
 
@@ -28,10 +29,10 @@ class Submenu(Base):
 class Dish(Base):
     __tablename__ = 'dish'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String, default=str(uuid.uuid4()), primary_key=True)
     name = Column(String, nullable=False)
     price = Column(Numeric(precision=8, scale=2), nullable=False)
-    submenu_id = Column(Integer, ForeignKey('submenu.id'), nullable=False)
+    submenu_id = Column(String, ForeignKey('submenu.id'), nullable=False)
     submenu = relationship('Submenu', back_populates='dishes')
 
 
