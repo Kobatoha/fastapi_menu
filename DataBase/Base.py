ac1947070452bd4bd3fbedd5d0de1084d736469a
Alexandra <kobatoha@gmail.com>
2024-01-26 21:13:48 +0300

    add get_db

--- DataBase/Base.py
+++ DataBase/Base.py
@@ -1,3 +1,17 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+from config import DB_URL
+
+
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
